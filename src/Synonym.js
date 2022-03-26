36f121540b99081a4ece05d0168670ab3342e310
Faerthurin <97044699+Faerthurin@users.noreply.github.com>
2022-03-26 21:24:16 +0100

    changed some styling

--- src/Synonym.js
+++ src/Synonym.js
@@ -1,9 +1,11 @@
 import React from "react";
 
+import "./Synonym.css";
+
 export default function Synonym(props) {
   if (props.synonyms) {
     return (
-      <ul className="Synonyms">
+      <ul className="synonyms">
         {props.synonyms.map(function (synonym, index) {
           return <ol key={index}>{synonym}</ol>;
         })}
